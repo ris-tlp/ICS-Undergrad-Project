a19eeba1051ab5db6d921a5cee2e10ee8d3366b2
Omar Pervez Khan <omarkhantlp@gmail.com>
2018-04-29 21:45:41 +0300

    -

--- src/com/omarkhan/Main.java
+++ src/com/omarkhan/Main.java
@@ -1,8 +1,94 @@
 package com.omarkhan;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Test");
-    }
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+
+//        String filepath = "C:\\workspace\\ICS-Undergrad-Project\\userbase.txt";
+//        Scanner scan = new Scanner(new File("userbase.txt"));
+
+//        File file = new File("C:\\workspace\\ICS-Undergrad-Project\\userbase.txt");
+//        Scanner scan = new Scanner(file);
+
+        Scanner scan = null;
+        try {
+            scan = new Scanner (new File("userbase.txt"));
+
+
+        } catch(FileNotFoundException e) {
+        }
+
+        student[] stud = new student[5];
+
+        for(int i = 0; i < stud.length; i++)
+        {
+            int ID = scan.nextInt();
+            String name = scan.next();
+            String password = scan.next();
+            int max = scan.nextInt();
+            int attempts = scan.nextInt();
+
+
+            stud[i] = new student(ID, name, password, max, attempts);
+//            System.out.println(scan.nextLine());
+
+        }
+
+        System.out.println("Welcome to the online test system.");
+        login(stud);
+
+        }
+
+        private static void login(student[] stewd)
+        {
+
+            String name;
+            String password;
+
+            System.out.println("Please enter your name and your password.");
+
+            System.out.println("Name: ");
+            name = s.nextLine();
+
+            System.out.println("Password: ");
+            password = s.nextLine();
+
+            boolean isValid = verifyLogin(name,password,stewd);
+            while (isValid == false)
+            {
+                System.out.println("Incorrect login, please try again.");
+
+                System.out.println("Name: ");
+                name = s.nextLine();
+
+                System.out.println("Password: ");
+                password = s.nextLine();
+
+                verifyLogin(name,password,stewd);
+
+            }
+
+        }
+
+        private static boolean verifyLogin(String username, String password, student[] s)
+        {
+            for (int i = 0; i < s.length; i++)
+            {
+                if (username.equals(s[i].getName()) && password.equals(s[i].getPassword()))
+                {
+                    System.out.println("Successful login!");
+                    //invoke other shit now
+                    return true;
+                }
+            }
+            return false;
+        }
 }
---
+++ src/com/omarkhan/student.java
@@ -0,0 +1,39 @@
+package com.omarkhan;
+
+
+public class student {
+
+    private int ID;
+    private String name;
+    private String password;
+    private int highestScore;
+    private int attempts;
+
+    public student(int ID, String name, String password, int highestScore, int attempts) {
+        this.ID = ID;
+        this.name = name;
+        this.password = password;
+        this.highestScore = highestScore;
+        this.attempts = attempts;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getHighestScore() {
+        return highestScore;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+}
